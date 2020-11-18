173fa4c000bb2ba8432b185b7fa2318c90122b83
Esteban SAINZ <esteban.sainz@etu.u-bordeaux.fr>
2020-11-18 10:42:11 +0100

    Display Monster

--- src/main/java/fr/ubx/poo/game/World.java
+++ src/main/java/fr/ubx/poo/game/World.java
@@ -6,7 +6,9 @@ package fr.ubx.poo.game;
 
 import fr.ubx.poo.model.decor.Decor;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
@@ -41,6 +43,18 @@ public class World {
             }
         }
         throw new PositionNotFoundException("Princess");
+    }
+
+    public List<Position> findMonsters() {
+        List<Position> positions = new ArrayList<>();
+        for (int x = 0; x < dimension.width; x++) {
+            for (int y = 0; y < dimension.height; y++) {
+                if (raw[y][x] == WorldEntity.Monster) {
+                    positions.add(new Position(x, y));
+                }
+            }
+        }
+        return positions;
     }
 
     public Decor get(Position position) {
